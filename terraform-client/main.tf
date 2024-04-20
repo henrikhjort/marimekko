27d07033e7bd3ce8bf16cc4f2a408df646396af3
Henrik <henrik@etufillari.com>
2024-04-20 14:53:39 +0300

    frontend terraform config

---
+++ terraform-client/main.tf
@@ -0,0 +1,44 @@
+# Specify the required Terraform and Provider settings
+terraform {
+  required_providers {
+    vercel = {
+      source  = "vercel/vercel"
+      version = "~> 0.3"
+    }
+  }
+}
+
+provider "vercel" {
+  api_token = var.api_token
+}
+
+# Resource to create a Vercel project
+resource "vercel_project" "marimekko" {
+  name      = "${var.project_name}"
+  framework = "nextjs"
+  team_id   = "${var.team_id}"
+
+  git_repository = {
+    type = "github"
+    repo = "${var.repository_name}"
+    ref  = "${var.source_branch}"
+  }
+
+  root_directory = "${var.project_root}"
+}
+
+output "vercel_project_id" {
+  value = vercel_project.marimekko.id
+}
+
+output "vercel_team_id" {
+  value = "${var.team_id}"
+}
+
+resource "vercel_project_environment_variable" "prod_api_url" {
+  project_id = vercel_project.marimekko.id
+  team_id   = "${var.team_id}"
+  key        = "NEXT_PUBLIC_API_URL"
+  value      = "${var.prod_api_url}"
+  target     = ["production"]
+}
---
+++ terraform-client/variables.tf
@@ -0,0 +1,36 @@
+# terraform/variables.tf
+
+variable "project_name" {
+  type = string
+  description = "Project name"
+}
+
+variable "team_id" {
+  type = string
+  description = "Vercel team ID"
+}
+
+variable "project_root" {
+  type = string
+  description = "Project source directory"
+}
+
+variable "source_branch" {
+  type = string
+  description = "Branch to trigger deployments"
+}
+
+variable "repository_name" {
+  type = string
+  description = "Repository name"
+}
+
+variable "api_token" {
+  type = string
+  description = "Vercel API token"
+}
+
+variable "prod_api_url" {
+  type = string
+  description = "API URL for the prod environment"
+}
