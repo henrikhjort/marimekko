b918756bdad1102e57d9ce4fc9abc91131cf3bd7
Henrik <henrik@etufillari.com>
2024-04-20 13:43:40 +0300

    trigger deployment

--- functions/api-test/index.ts
+++ functions/api-test/index.ts
@@ -6,9 +6,7 @@ const httpTrigger: AzureFunction = async function (
 ): Promise<void> {
   context.log("HTTP trigger function processed a request.");
   const name = req.query.name || (req.body && req.body.name);
-  const responseMessage = name
-    ? "Hello, " + name + ". This HTTP triggered function executed successfully."
-    : "This HTTP triggered function executed successfully. Pass a name in the query string or in the request body for a personalized response.";
+  const responseMessage = "hello world";
 
   context.res = {
     // status: 200, /* Defaults to 200 */
